7190e112e2437d800445a9fa640a9ed362a50f70
vaca <wsmslgh@gmail.com>
2022-05-05 06:27:45 -0700

    fdg54

--- main/i2c_simple_main.c
+++ main/i2c_simple_main.c
@@ -19,86 +19,89 @@
 #include <stdio.h>
 #include "esp_log.h"
 #include "driver/i2c.h"
+#include "max30102.h"
 
 static const char *TAG = "i2c-simple-example";
 
-#define I2C_MASTER_SCL_IO           CONFIG_I2C_MASTER_SCL      /*!< GPIO number used for I2C master clock */
-#define I2C_MASTER_SDA_IO           CONFIG_I2C_MASTER_SDA      /*!< GPIO number used for I2C master data  */
-#define I2C_MASTER_NUM              0                          /*!< I2C master i2c port number, the number of i2c peripheral interfaces available will depend on the chip */
-#define I2C_MASTER_FREQ_HZ          400000                     /*!< I2C master clock frequency */
-#define I2C_MASTER_TX_BUF_DISABLE   0                          /*!< I2C master doesn't need buffer */
-#define I2C_MASTER_RX_BUF_DISABLE   0                          /*!< I2C master doesn't need buffer */
-#define I2C_MASTER_TIMEOUT_MS       1000
+#define GPIO_INPUT_IO_0     4
+#define GPIO_INPUT_PIN_SEL  ((1ULL<<GPIO_INPUT_IO_0))
+#define ESP_INTR_FLAG_DEFAULT 1
 
-#define MAX30102ADDR    (0xae>>1)
+static xQueueHandle gpio_evt_queue = NULL;
 
-static esp_err_t i2c_master_init(void)
+#define CACHE_NUMS 150
+#define PPG_DATA_THRESHOLD 100000
+uint8_t max30102_int_flag=0;
+
+float ppg_data_cache_RED[CACHE_NUMS]={0};
+float ppg_data_cache_IR[CACHE_NUMS]={0};
+
+
+static void IRAM_ATTR gpio_isr_handler(void* arg)
 {
-    int i2c_master_port = I2C_MASTER_NUM;
-
-    i2c_config_t conf = {
-        .mode = I2C_MODE_MASTER,
-        .sda_io_num = I2C_MASTER_SDA_IO,
-        .scl_io_num = I2C_MASTER_SCL_IO,
-        .sda_pullup_en = GPIO_PULLUP_ENABLE,
-        .scl_pullup_en = GPIO_PULLUP_ENABLE,
-        .master.clk_speed = I2C_MASTER_FREQ_HZ,
-    };
-
-    i2c_param_config(i2c_master_port, &conf);
-
-    return i2c_driver_install(i2c_master_port, conf.mode, I2C_MASTER_RX_BUF_DISABLE, I2C_MASTER_TX_BUF_DISABLE, 0);
-}
-
-
-
-
-
-
-
-static esp_err_t wr_max30102_one_data(uint8_t address,uint8_t saddress,uint8_t w_data )
-{
-    uint8_t write_buf[2] = {saddress,w_data};
-    return  i2c_master_write_to_device(I2C_MASTER_NUM, address,write_buf, sizeof(write_buf), I2C_MASTER_TIMEOUT_MS / portTICK_RATE_MS);
-}
-
-static esp_err_t rd_max30102_one_data(uint8_t address,uint8_t saddress,uint8_t *r_data)
-{
-    return i2c_master_write_read_device(I2C_MASTER_NUM, address, &saddress, 1, r_data, 1, I2C_MASTER_TIMEOUT_MS / portTICK_RATE_MS);
+    uint32_t gpio_num = (uint32_t) arg;
+    xQueueSendFromISR(gpio_evt_queue, &gpio_num, NULL);
 }
 
 void app_main(void)
 {
-    uint8_t data[2];
-    ESP_ERROR_CHECK(i2c_master_init());
-    ESP_LOGI(TAG, "I2C initialized successfully");
+    gpio_config_t io_conf = {};
+    io_conf.intr_type = GPIO_INTR_NEGEDGE;
+    //bit mask of the pins, use GPIO4/5 here
+    io_conf.pin_bit_mask = GPIO_INPUT_PIN_SEL;
+    //set as input mode
+    io_conf.mode = GPIO_MODE_INPUT;
+    //enable pull-up mode
+    io_conf.pull_up_en = 1;
+    gpio_config(&io_conf);
 
-    uint8_t rda;
+
+    //create a queue to handle gpio event from isr
+    gpio_evt_queue = xQueueCreate(10, sizeof(uint32_t));
+    //install gpio isr service
+    gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
+    //hook isr handler for specific gpio pin
+    gpio_isr_handler_add(GPIO_INPUT_IO_0, gpio_isr_handler, (void*) GPIO_INPUT_IO_0);
+    vTaskDelay(100);
 
 
-    while (1){
-        ESP_ERROR_CHECK(wr_max30102_one_data(MAX30102ADDR,0x09,0x0b));
-        ESP_ERROR_CHECK(rd_max30102_one_data(MAX30102ADDR,0xff,&rda));
-        ESP_LOGE("fuck","max30102 ID =%d\n",rda);
+    max30102_init();
+    uint16_t cache_counter=0;  //»º´æ¼ÆÊýÆ÷
+    float max30102_data[2],fir_output[2];
+    uint32_t io_num;
+    for(;;) {
+//        if(xQueueReceive(gpio_evt_queue, &io_num, portMAX_DELAY)) {
+//            ESP_LOGE("fuck","GPIO[%d] intr, val: %d\n", io_num, gpio_get_level(io_num));
+//            vTaskDelay(1);
+//        }
 
-        ESP_ERROR_CHECK(wr_max30102_one_data(MAX30102ADDR,0x08,0x43));
+        max30102_fifo_read(max30102_data);		//¶ÁÈ¡Êý¾Ý
+        fir_output[0]=max30102_data[0];
+        fir_output[1]=max30102_data[1];
 
-        ESP_ERROR_CHECK(wr_max30102_one_data(MAX30102ADDR,0x21,0x01));
-        vTaskDelay(5);
-        ESP_ERROR_CHECK(rd_max30102_one_data(MAX30102ADDR,0x1F,&rda));
-        ESP_LOGE("fuck","temp1=%d\n",rda);
-        int temp1=rda;
-        ESP_ERROR_CHECK(rd_max30102_one_data(MAX30102ADDR,0x20,&rda));
-        ESP_LOGE("fuck","temp2=%d\n",rda);
-        int temp2=rda;
-        float temp=temp1+(temp2*0.0625);
-        ESP_LOGE("fuck","Temperature=%.4f\n",temp);
-        vTaskDelay(100);
+
+
+
+        if((max30102_data[0]>PPG_DATA_THRESHOLD)&&(max30102_data[1]>PPG_DATA_THRESHOLD))
+        {
+            ppg_data_cache_IR[cache_counter]=fir_output[0];
+            ppg_data_cache_RED[cache_counter]=fir_output[1];
+            cache_counter++;
+        }
+        else
+        {
+            cache_counter=0;
+        }
+
+
+        if(cache_counter>=CACHE_NUMS)
+        {
+            printf("heart rate %d/min   ",max30102_getHeartRate(ppg_data_cache_IR,CACHE_NUMS));
+            printf("o2  %.2f\n",max30102_getSpO2(ppg_data_cache_IR,ppg_data_cache_RED,CACHE_NUMS));
+            cache_counter=0;
+        }
+        vTaskDelay(1);
     }
-
-
-
-
 
 
 }
---
+++ main/max30102.c
@@ -0,0 +1,161 @@
+
+#include <stdint-gcc.h>
+
+#include <driver/i2c.h>
+#include "max30102.h"
+
+
+static const char *TAG = "i2c-simple-example";
+
+#define I2C_MASTER_SCL_IO           CONFIG_I2C_MASTER_SCL      /*!< GPIO number used for I2C master clock */
+#define I2C_MASTER_SDA_IO           CONFIG_I2C_MASTER_SDA      /*!< GPIO number used for I2C master data  */
+#define I2C_MASTER_NUM              0                          /*!< I2C master i2c port number, the number of i2c peripheral interfaces available will depend on the chip */
+#define I2C_MASTER_FREQ_HZ          400000                     /*!< I2C master clock frequency */
+#define I2C_MASTER_TX_BUF_DISABLE   0                          /*!< I2C master doesn't need buffer */
+#define I2C_MASTER_RX_BUF_DISABLE   0                          /*!< I2C master doesn't need buffer */
+#define I2C_MASTER_TIMEOUT_MS       1000
+
+#define MAX30102ADDR    (0xae>>1)
+
+static esp_err_t i2c_master_init(void) {
+    int i2c_master_port = I2C_MASTER_NUM;
+
+    i2c_config_t conf = {
+            .mode = I2C_MODE_MASTER,
+            .sda_io_num = I2C_MASTER_SDA_IO,
+            .scl_io_num = I2C_MASTER_SCL_IO,
+            .sda_pullup_en = GPIO_PULLUP_ENABLE,
+            .scl_pullup_en = GPIO_PULLUP_ENABLE,
+            .master.clk_speed = I2C_MASTER_FREQ_HZ,
+    };
+
+    i2c_param_config(i2c_master_port, &conf);
+
+    return i2c_driver_install(i2c_master_port, conf.mode, I2C_MASTER_RX_BUF_DISABLE, I2C_MASTER_TX_BUF_DISABLE, 0);
+}
+
+static esp_err_t wr_max30102_one_data(uint8_t saddress, uint8_t w_data) {
+    uint8_t write_buf[2] = {saddress, w_data};
+    return i2c_master_write_to_device(I2C_MASTER_NUM, MAX30102ADDR,
+                                      write_buf, sizeof(write_buf),
+                                      I2C_MASTER_TIMEOUT_MS / portTICK_RATE_MS);
+}
+
+static esp_err_t rd_max30102_data(uint8_t saddress, uint8_t *r_data, uint8_t data_size) {
+    return i2c_master_write_read_device(I2C_MASTER_NUM, MAX30102ADDR,
+                                        &saddress, 1,
+                                        r_data, data_size,
+                                        I2C_MASTER_TIMEOUT_MS / portTICK_RATE_MS);
+}
+
+
+void max30102_i2c_write(uint8_t reg_adder, uint8_t data) {
+    uint8_t transmit_data[2];
+    transmit_data[0] = reg_adder;
+    transmit_data[1] = data;
+    ESP_ERROR_CHECK(wr_max30102_one_data(reg_adder, data));
+}
+
+void max30102_i2c_read(uint8_t reg_adder, uint8_t *pdata, uint8_t data_size) {
+    ESP_ERROR_CHECK(rd_max30102_data(reg_adder, pdata, data_size));
+}
+
+void max30102_init(void) {
+    i2c_master_init();
+    uint8_t data;
+    vTaskDelay(1);
+    max30102_i2c_write(MODE_CONFIGURATION, 0x40);  //reset the device
+
+    vTaskDelay(1);
+
+    max30102_i2c_write(INTERRUPT_ENABLE1, 0xE0);
+    max30102_i2c_write(INTERRUPT_ENABLE2, 0x00);  //interrupt enable: FIFO almost full flag, new FIFO Data Ready,
+    //                   ambient light cancellation overflow, power ready flag,
+    //						    		internal temperature ready flag
+
+    max30102_i2c_write(FIFO_WR_POINTER, 0x00);
+    max30102_i2c_write(FIFO_OV_COUNTER, 0x00);
+    max30102_i2c_write(FIFO_RD_POINTER, 0x00);   //clear the pointer
+
+    max30102_i2c_write(FIFO_CONFIGURATION,
+                       0x4F); //FIFO configuration: sample averaging(1),FIFO rolls on full(0), FIFO almost full value(15 empty data samples when interrupt is issued)
+
+    max30102_i2c_write(MODE_CONFIGURATION, 0x03);  //MODE configuration:SpO2 mode
+
+    max30102_i2c_write(SPO2_CONFIGURATION,
+                       0x2A); //SpO2 configuration:ACD resolution:15.63pA,sample rate control:200Hz, LED pulse width:215 us
+
+    max30102_i2c_write(LED1_PULSE_AMPLITUDE, 0x2f);    //IR LED
+    max30102_i2c_write(LED2_PULSE_AMPLITUDE, 0x2f); //RED LED current
+
+    max30102_i2c_write(TEMPERATURE_CONFIG, 0x01);   //temp
+
+    max30102_i2c_read(INTERRUPT_STATUS1, &data, 1);
+    max30102_i2c_read(INTERRUPT_STATUS2, &data, 1);  //clear status
+
+
+}
+
+
+void max30102_fifo_read(float *output_data) {
+    uint8_t receive_data[6];
+    uint32_t data[2];
+    max30102_i2c_read(FIFO_DATA, receive_data, 6);
+    data[0] = ((receive_data[0] << 16 | receive_data[1] << 8 | receive_data[2]) & 0x03ffff);
+    data[1] = ((receive_data[3] << 16 | receive_data[4] << 8 | receive_data[5]) & 0x03ffff);
+    *output_data = data[0];
+    *(output_data + 1) = data[1];
+}
+
+uint16_t max30102_getHeartRate(float *input_data, uint16_t cache_nums) {
+    float input_data_sum_aver = 0;
+    uint16_t i, temp=0;
+
+
+    for (i = 0; i < cache_nums; i++) {
+        input_data_sum_aver += *(input_data + i);
+    }
+    input_data_sum_aver = input_data_sum_aver / cache_nums;
+    for (i = 0; i < cache_nums; i++) {
+        if ((*(input_data + i) > input_data_sum_aver) && (*(input_data + i + 1) < input_data_sum_aver)) {
+            temp = i;
+            break;
+        }
+    }
+    i++;
+    for (; i < cache_nums; i++) {
+        if ((*(input_data + i) > input_data_sum_aver) && (*(input_data + i + 1) < input_data_sum_aver)) {
+            temp = i - temp;
+            break;
+        }
+    }
+    if ((temp > 14) && (temp < 100)) {
+        return 3000 / temp;
+    } else {
+        return 0;
+    }
+}
+
+float max30102_getSpO2(float *ir_input_data, float *red_input_data, uint16_t cache_nums) {
+    float ir_max = *ir_input_data, ir_min = *ir_input_data;
+    float red_max = *red_input_data, red_min = *red_input_data;
+    float R;
+    uint16_t i;
+    for (i = 1; i < cache_nums; i++) {
+        if (ir_max < *(ir_input_data + i)) {
+            ir_max = *(ir_input_data + i);
+        }
+        if (ir_min > *(ir_input_data + i)) {
+            ir_min = *(ir_input_data + i);
+        }
+        if (red_max < *(red_input_data + i)) {
+            red_max = *(red_input_data + i);
+        }
+        if (red_min > *(red_input_data + i)) {
+            red_min = *(red_input_data + i);
+        }
+    }
+
+    R = ((ir_max + ir_min) * (red_max - red_min)) / ((red_max + red_min) * (ir_max - ir_min));
+    return ((-45.060) * R * R + 30.354 * R + 94.845);
+}
