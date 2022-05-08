38a2da12517d8368887d1087f577f074dd3e9090
vaca <wsmslgh@gmail.com>
2022-05-07 20:36:36 -0700

    fdg54redfhygfedyhgedrddhghgdfgfddr5ft

--- main/i2c_simple_main.c
+++ main/i2c_simple_main.c
@@ -20,8 +20,41 @@
 #include "esp_log.h"
 #include "driver/i2c.h"
 #include "max30102.h"
-
+#include "dsps_fir.h"
 static const char *TAG = "i2c-simple-example";
+
+
+
+
+
+
+
+#define BLOCK_SIZE           1
+#define NUM_TAPS             29
+
+uint32_t blockSize = BLOCK_SIZE;
+uint32_t numBlocks = BLOCK_SIZE;
+fir_f32_t *S_red;
+fir_f32_t *S_ir;
+static float firStateF32_ir[BLOCK_SIZE + NUM_TAPS - 1];
+static float firStateF32_red[BLOCK_SIZE + NUM_TAPS - 1];
+
+
+const float firCoeffs32LP[NUM_TAPS] = {
+        -0.001542701735,-0.002211477375,-0.003286228748, -0.00442651147,-0.004758632276,
+        -0.003007677384, 0.002192312852,  0.01188309677,  0.02637642808,  0.04498152807,
+        0.06596207619,   0.0867607221,   0.1044560149,   0.1163498312,   0.1205424443,
+        0.1163498312,   0.1044560149,   0.0867607221,  0.06596207619,  0.04498152807,
+        0.02637642808,  0.01188309677, 0.002192312852,-0.003007677384,-0.004758632276,
+        -0.00442651147,-0.003286228748,-0.002211477375,-0.001542701735
+};
+
+
+
+
+
+
+
 
 #define GPIO_INPUT_IO_0     4
 #define GPIO_INPUT_PIN_SEL  ((1ULL<<GPIO_INPUT_IO_0))
@@ -42,6 +75,10 @@ static void IRAM_ATTR gpio_isr_handler(void *arg) {
 }
 
 void app_main(void) {
+    dsps_fir_init_f32(S_ir, firCoeffs32LP, firStateF32_ir, NUM_TAPS);
+    dsps_fir_init_f32(S_red, firCoeffs32LP, firStateF32_red, NUM_TAPS);
+
+
     gpio_config_t io_conf = {};
     io_conf.intr_type = GPIO_INTR_NEGEDGE;
     //bit mask of the pins, use GPIO4/5 here
@@ -67,9 +104,10 @@ void app_main(void) {
         if (fuck) {
             fuck = 0;
             max30102_fifo_read(max30102_data);
-            fir_output[0] = max30102_data[0];
-            fir_output[1] = max30102_data[1];
-
+//            fir_output[0] = max30102_data[0];
+//            fir_output[1] = max30102_data[1];
+            dsps_fir_f32_ansi(S_ir, &max30102_data[0], &fir_output[0], 1);
+            dsps_fir_f32_ansi(S_red, &max30102_data[1], &fir_output[1], 1);
 
             if ((max30102_data[0] > PPG_DATA_THRESHOLD) && (max30102_data[1] > PPG_DATA_THRESHOLD)) {
                 ppg_data_cache_IR[cache_counter] = fir_output[0];
